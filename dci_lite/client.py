b1c8c6cb191b15d4e2898a3cf1f0f77604d865a5
Gonéri Le Bouder <goneri@redhat.com>
2018-10-07 01:22:10 -0400

    functional test

--- dci_lite/client.py
+++ dci_lite/client.py
@@ -3,23 +3,9 @@ import dateutil.parser
 from dciclient.v1.api import context as dci_context
 from dciclient.v1.api import base as dci_base
 
-import json.decoder
 import os
+import json
 
-class Cache():
-    def __init__(self):
-        self._data = {}
-    def set(self, resource, item_id, obj):
-        if resource not in self._data:
-            self._data[resource] = {}
-        self._data[resource][item_id] = obj
-        return obj
-
-    def get(self, resource, item_id):
-        return self._data[resource][item_id]
-
-
-cache = Cache()
 
 class DCILiteNotFound(Exception):
     pass
@@ -29,38 +15,56 @@ class DCILiteDeleteFailure(Exception):
     pass
 
 
+def kwargs_to_data(kwargs):
+    # Handle the FK
+    data = {}
+    for i in kwargs:
+        if hasattr(kwargs[i], 'id'):
+            data[i + '_id'] = kwargs[i].id
+        else:
+            data[i] = kwargs[i]
+    return data
+
+
 class DCIResource():
-    def __init__(self, transport, resource, data, parent_resource=None, subresource=None):
+    def __init__(self, transport, resource, data,
+                 parent_resource=None, subresource=None):
         self._transport = transport
         self._resource = resource
         self._parent_resource = parent_resource
         self._subresource = subresource
         self._uri = self._build_uri()
-        self._data = data
+        self._data = {}
+        self._fk = {}
         self._new_data = {}
+        self._load_data(data)
 
     @classmethod
     def from_id(cls, transport, resource, item_id, **kwargs):
         uri = '%s/%s/%s' % (transport.dci_cs_api, resource, item_id)
-        try:
-            return cache.get(resource, item_id)
-        except KeyError:
-            pass
         r = transport.get(uri, timeout=dci_base.HTTP_TIMEOUT, params=kwargs)
         if r.status_code == 404:
-            raise DCILiteNotFound('resource not found at %s: %s' % (uri, r.text))
+            msg = 'resource not found at %s: %s' % (uri, r.text)
+            raise DCILiteNotFound(msg)
         if r.status_code != 200:
             raise(Exception('Failed to get resource %s: %s' % (uri, r.text)))
         obj = cls(transport, resource, list(r.json().values())[0])
-        cache.set(resource, item_id, obj)
         return obj
 
-    # TODO: DUP in DCIResourceCollection
     def _build_uri(self):
         if self._subresource:
-            return '%s/%s/%s/%s' % (self._transport.dci_cs_api, self._resource, self._parent_resource.id(), self._subresource)
+            return '%s/%s/%s/%s' % (
+                self._transport.dci_cs_api,
+                self._resource,
+                self._parent_resource.id,
+                self._subresource)
         else:
             return '%s/%s' % (self._transport.dci_cs_api, self._resource)
+
+    def refresh(self):
+        uri = '%s/%s' % (self._uri, self.id)
+        r = self._transport.get(uri)
+        self._load_data(list(r.json().values())[0])
 
     def commit(self):
         """Update a specific resource"""
@@ -71,19 +75,25 @@ class DCIResource():
         r = self._transport.put(uri, timeout=dci_base.HTTP_TIMEOUT,
                                 headers={'If-match': self._data['etag']},
                                 json=self._new_data)
-        if r.status_code != 204:
-            raise(Exception(r.text))
+        if r.status_code != 200:
+            msg = 'Failed to commit object %s: %s' % (uri, r.text)
+            raise(Exception(msg))
+        self._load_data(list(r.json().values())[0])
 
-        # Reinitialize the object
-        for i in self._new_data:
-            self._data[i] = self._new_data[i]
+    def _load_data(self, data):
+        for i in data:
+            if i in ('created_at', 'updated_at'):
+                self._data[i] = dateutil.parser.parse(data[i])
+            else:
+                self._data[i] = data[i]
         self._new_data = {}
-        return r
-
 
     def download(self, target):
         uri = self._uri + '/' + self._data['id'] + '/content'
-        r = self._transport.get(uri, stream=True, timeout=dci_base.HTTP_TIMEOUT)
+        r = self._transport.get(
+            uri,
+            stream=True,
+            timeout=dci_base.HTTP_TIMEOUT)
         r.raise_for_status()
         with open(target + '.part', 'wb') as f:
             for chunk in r.iter_content(chunk_size=1024):
@@ -92,36 +102,46 @@ class DCIResource():
         os.rename(target + '.part', target)
         return r
 
-
     def __str__(self):
         return "resource %s\ndata: %s\nuncommited data: %s" % (
             self._uri, self._data, self._new_data)
 
-
     def __getattr__(self, name):
         def return_func(name, params={}):
-            def attribute(*v):
-                if len(v) == 1:
-                    self._data[name] = v[0]
-                    self._new_data[name] = v[0]
-                # Return a datetime object
-                if name in ('created_at', 'updated_at'):
-                    return dateutil.parser.parse(self._data[name])
-                return self._data[name]
-
-            if name in self._data:
-                return attribute
-            elif name + '_id' in self._data:
+            if name + '_id' in self.__dict__['_data']:
                 guessed_resource_name = name + 's'
                 # NOTE: We should be able to just create a new DCIResource
                 # instance here.
-                return DCIResource.from_id(self._transport, resource=guessed_resource_name, item_id=self._data[name + '_id'])
+                if name not in self.__dict__['_fk']:
+                    self.__dict__['_fk'][name] = DCIResource.from_id(
+                        self._transport,
+                        resource=guessed_resource_name,
+                        item_id=self.__dict__['_data'][name + '_id'])
+                return self.__dict__['_fk'][name]
             else:
-                return DCIResourceCollection(self._transport, self._resource, parent_resource=self, subresource=name)
+                if name not in self.__dict__['_fk']:
+                    self.__dict__['_fk'][name] = DCIResourceCollection(
+                        self._transport,
+                        self._resource,
+                        parent_resource=self,
+                        subresource=name)
+                return self.__dict__['_fk'][name]
+
+        if name.startswith('_'):
+            return self.__dict__[name]
+        elif name in self.__dict__['_data']:
+            return self.__dict__['_data'][name]
         return return_func(name)
 
+    def __setattr__(self, name, value):
+        if name.startswith('_'):
+            self.__dict__[name] = value
+            return
+        self._data[name] = value
+        self._new_data[name] = value
+
     def delete(self):
-        uri = self._uri + '/' + self._data['id']
+        uri = self._uri + '/' + self.id
         r = self._transport.delete(
             uri, timeout=dci_base.HTTP_TIMEOUT,
             headers={'If-match': self._data['etag']})
@@ -129,31 +149,33 @@ class DCIResource():
             raise DCILiteDeleteFailure('failed to delete at %s: %s' % (
                 uri, r.text))
 
+    # Component
+    def file_upload(self, file_path):
+        uri = '%s/%s/files' % (self._uri, self.id)
+        with open(file_path, 'rb') as f:
+            return self._transport.post(uri, data=f)
+
 
 class DCIResourceCollection:
-    def __init__(self, transport, resource, parent_resource=None, subresource=None):
+    def __init__(self, transport, resource,
+                 parent_resource=None, subresource=None):
         self._transport = transport
         self._resource = resource
         self._parent_resource = parent_resource
         self._subresource = subresource
         self._uri = self._build_uri()
 
-
-    def _kwargs_to_data(self, kwargs):
-        # Handle the FK
-        data = {}
-        for i in kwargs:
-            if hasattr(kwargs[i], 'id'):
-                data[i + '_id'] = kwargs[i].id()
-            else:
-                data[i] = kwargs[i]
-        return data
-
     def _build_uri(self):
         if self._subresource:
-            return '%s/%s/%s/%s' % (self._transport.dci_cs_api, self._resource, self._parent_resource.id(), self._subresource)
+            return '%s/%s/%s/%s' % (
+                self._transport.dci_cs_api,
+                self._resource,
+                self._parent_resource.id,
+                self._subresource)
         else:
-            return '%s/%s' % (self._transport.dci_cs_api, self._resource)
+            return '%s/%s' % (
+                self._transport.dci_cs_api,
+                self._resource)
 
     def add(self, **kwargs):
         uri = self._uri
@@ -164,7 +186,7 @@ class DCIResourceCollection:
                 timeout=dci_base.HTTP_TIMEOUT,
                 data=kwargs['data'])
         else:
-            data = self._kwargs_to_data(kwargs)
+            data = kwargs_to_data(kwargs)
             r = self._transport.post(
                 uri,
                 timeout=dci_base.HTTP_TIMEOUT,
@@ -172,52 +194,59 @@ class DCIResourceCollection:
         if r.status_code != 201:
             raise(Exception('Failed to add %s: %s' % (uri, r.text)))
         if self._subresource:
-            return DCIResource(self._transport, self._resource, list(r.json().values())[0], parent_resource=self._parent_resource, subresource=self._subresource)
+            # return DCIResource(
+            #     self._transport,
+            #     self._resource,
+            #     list(r.json().values())[0],
+            #     parent_resource=self._parent_resource,
+            #     subresource=self._subresource)
         else:
-            return DCIResource(self._transport, self._resource, list(r.json().values())[0])
-
+            return DCIResource(
+                self._transport,
+                self._resource,
+                list(r.json().values())[0])
 
     def __iter__(self):
         return self.list()
 
-
     def __getitem__(self, item_id):
         return self.get(item_id)
-
 
     def __delitem__(self, item):
         self.delete(item)
 
-
     def get(self, item_id, **kwargs):
         uri = self._uri + '/' + item_id
-        try:
-            return cache.get(self._resource, item_id)
-        except KeyError:
-            pass
-        r = self._transport.get(uri, timeout=dci_base.HTTP_TIMEOUT, params=kwargs)
+        r = self._transport.get(
+            uri,
+            timeout=dci_base.HTTP_TIMEOUT,
+            params=kwargs)
         if r.status_code == 404:
-            raise DCILiteNotFound('resource not found at %s: %s' % (uri, r.text))
+            msg = 'resource not found at %s: %s' % (uri, r.text)
+            raise DCILiteNotFound(msg)
         if r.status_code != 200:
-            raise(Exception('Failed to get resource %s: %s' % (uri, r.text)))
-        obj = DCIResource(self._transport, self._resource, list(r.json().values())[0])
-        cache.set(self._resource, item_id, obj)
+            msg = 'Failed to get resource %s: %s' % (uri, r.text)
+            raise(Exception(msg))
+        obj = DCIResource(
+            self._transport,
+            self._resource,
+            list(r.json().values())[0])
         return obj
 
     def delete(self, item):
-        uri = self._uri + '/' + item
-        r = self._transport.delete(uri, timeout=dci_base.HTTP_TIMEOUT,
-                               headers={'If-match': item.etag()})
+        uri = self._uri + '/' + item.id
+        r = self._transport.delete(
+            uri,
+            timeout=dci_base.HTTP_TIMEOUT,
+            headers={'If-match': item.etag})
         if r.status_code != 204:
             raise DCILiteDeleteFailure('failed to delete at %s: %s' % (
                 uri, r.text))
         return r
 
-
     def first(self, **kwargs):
         gen = self.list(**kwargs)
         return next(gen)
-
 
     def find_or_add(self, **kwargs):
         try:
@@ -225,35 +254,33 @@ class DCIResourceCollection:
         except StopIteration:
             return self.add(**kwargs)
 
-
     def len(self, **kwargs):
         """List all resources"""
         uri = self._uri
-        data = self._kwargs_to_data(kwargs)
-        id = data.pop('id', None)
-        data['limit'] = data.get('limit', 1)
+        data = kwargs_to_data(kwargs)
+        data['limit'] = 1
 
-        # Type of resource that we will loop
-        resource_type = self._subresource or self._resource
-
-        r = self._transport.get(uri, timeout=dci_base.HTTP_TIMEOUT, params=data)
+        r = self._transport.get(
+            uri,
+            timeout=dci_base.HTTP_TIMEOUT,
+            params=data)
         if r.status_code == 404:
-            raise DCILiteNotFound('Resource not found at %s: %s' % (uri, r.text))
+            msg = 'Resource not found at %s: %s' % (uri, r.text)
+            raise DCILiteNotFound(msg)
         try:
                 j = r.json()
         except (ValueError):
-            raise Exception('Invalid answer from server for %s: %s' % (uri, r.text))
+            msg = 'Invalid answer from server for %s: %s' % (uri, r.text)
+            raise Exception(msg)
         return j['_meta']['count']
 
     def count(self, **kwargs):
         return self.len(**kwargs)
 
-
     def list(self, **kwargs):
         """List all resources"""
         uri = self._uri
-        data = self._kwargs_to_data(kwargs)
-        id = data.pop('id', None)
+        data = kwargs_to_data(kwargs)
         data['limit'] = data.get('limit', 1000)
 
         # Type of resource that we will loop
@@ -261,14 +288,19 @@ class DCIResourceCollection:
 
         data['offset'] = 0
         while True:
-            r = self._transport.get(uri, timeout=dci_base.HTTP_TIMEOUT, params=data)
+            r = self._transport.get(
+                uri,
+                timeout=dci_base.HTTP_TIMEOUT,
+                params=data)
             if r.status_code == 404:
-                raise DCILiteNotFound('resource not found at %s: %s' % (uri, r.text))
+                msg = 'resource not found at %s: %s' % (uri, r.text)
+                raise DCILiteNotFound(msg)
             try:
                 j = r.json()
                 del j['_meta']
             except (KeyError, ValueError):
-                raise Exception('Invalid answer from server for %s: %s' % (uri, r.text))
+                msg = 'Invalid answer from server for %s: %s' % (uri, r.text)
+                raise Exception(msg)
             items = list(j.values())[0]
             if not len(items):
                 break
@@ -276,13 +308,28 @@ class DCIResourceCollection:
                 yield DCIResource(self._transport, resource_type, i)
             data['offset'] += data['limit']
 
-
     def purge(self):
         uri = self._uri + '/purge'
         r = self._transport.post(uri, timeout=dci_base.HTTP_TIMEOUT)
         if r.status_code != 204:
             raise(Exception('Failed to purge resource %s: %s' % (uri, r.text)))
         return r
+
+    # Job
+    def schedule(self, **kwargs):
+        uri = self._uri + '/schedule'
+        r = self._transport.post(
+            uri,
+            timeout=dci_base.HTTP_TIMEOUT,
+            data=json.dumps(kwargs_to_data(kwargs)))
+        if r.status_code != 201:
+            msg = 'Failed to schedule a new job %s: %s' % (uri, r.text)
+            raise(Exception(msg))
+        obj = DCIResource(
+            self._transport,
+            self._resource,
+            list(r.json().values())[0])
+        return obj
 
 
 class Transport:
@@ -310,11 +357,25 @@ class Transport:
 
 class DCIClient:
 
-    def __init__(self, dci_login=None, dci_password=None, dci_cs_url=None):
-        context = dci_context.build_dci_context(dci_login=dci_login,
-                                                dci_password=dci_password,
-                                                dci_cs_url=dci_cs_url)
-        self._transport = Transport(context)
+    def __init__(self, transport):
+        self._transport = transport
+
+    @classmethod
+    def for_user(cls, dci_login=None, dci_password=None, dci_cs_url=None):
+        context = dci_context.build_dci_context(
+            dci_login=dci_login,
+            dci_password=dci_password,
+            dci_cs_url=dci_cs_url)
+        return cls(Transport(context))
+
+    @classmethod
+    def for_remoteci(cls, dci_client_id=None, dci_api_secret=None,
+                     dci_cs_url=None):
+        context = dci_context.build_signature_context(
+            dci_cs_url=dci_cs_url,
+            dci_client_id=dci_client_id,
+            dci_api_secret=dci_api_secret)
+        return cls(Transport(context))
 
     def __getattr__(self, resource):
         def return_collection(params={}):
