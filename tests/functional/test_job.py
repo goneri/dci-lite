4e9bec401cd183c2fa01db76a5bec1165c401ec0
Gonéri Le Bouder <goneri@redhat.com>
2018-11-13 15:44:42 -0500

    functional: open as binary to avoid a warning
    Otherwise, Request raises a warning when it computes the size of the
    file.

--- tests/functional/test_job.py
+++ tests/functional/test_job.py
@@ -3,7 +3,7 @@
 
 def test_schedule(c, c_rci, my_topic):
     new_component = c.components.add(name='foo', type='aa', topic=my_topic)
-    with open('/etc/fstab') as fd:
+    with open('/etc/fstab', 'rb') as fd:
         my_file = new_component.files.add(data=fd)
     assert my_file.id
 
